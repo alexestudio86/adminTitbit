20d20c3bffa3d99ad3c64a60a637af8864b515c7
Alejandro R <alexestudio86@gmail.com>
2022-07-13 10:58:13 -0500

    prueba

--- src/components/OrderCreator.vue
+++ src/components/OrderCreator.vue
@@ -1,38 +1,39 @@
 <template>
     <div>
         <div class='w3-white p-3'>
-            <!-- NOMBRES Y VARIANTES -->
             <form v-on:submit.prevent>
                 <div class='mb-3'>
                     <span>*Orden</span>
                     <div class="tooltip">
                         <span class="tooltiptext px-1" id='orderTooltip'>Elija una opción</span>
                     </div>
-                    <select class='w3-select' name='nameOrders' id="nameOrders" v-model='orderTitle' v-on:change='checkVariants'>
+                    <!-- NOMBRE -->
+                    <select class='w3-select' name='nameOrders' id="nameOrders" v-model='item.orderTitle' v-on:change='checkVariants'>
                         <option value="" disabled selected>Elige una opcion</option>
-                        <option v-for="(dish, index) of dishes" :key="index" v-bind:value="dish.title">{{ dish.title }}</option>
+                        <option v-for="(dish, index) of dishes" :key="index" :value="dish.title">{{ dish.title }}</option>
                     </select>
                     <div class='my-3' v-if='orderVariants.length > 0'>
                         <span>*Variante</span>
-                        <select class='w3-select' name="variantOrder" id="variantOrder" v-model='variantSelected' >
+                        <!-- VARIANTE -->
+                        <select class='w3-select' name="variantOrder" id="variantOrder" v-model='item.orderPrice' >
                             <option value="" disabled='disabled' selected='selected'>Elija una opción</option>
-                            <option v-for='(v, index) of orderVariants' :key="index" :value='v' >{{ v }}</option>
+                            <option v-for='(v, index) of orderVariants' :key="index" :value='v.quantity' @input="item.variantSelected" >{{ v.title }}</option>
                         </select>
                     </div>
-                    <!-- CANTIDAD -->
                     <div class='w3-row py-2'>
                         <div class="w3-col s8">
                             <div class="w3-row">
                                 <div class="w3-col s3">
-                                    <button class='w3-button w3-border w3-pale-green w-100' @click="orderQuantity > 1 ? orderQuantity-- : ''" >
+                                    <button class='w3-button w3-border w3-pale-green w-100' @click="item.orderQuantity > 1 ? item.orderQuantity-- : ''" >
                                         <i class="fa-solid fa-angle-down"></i>
                                     </button>
                                 </div>
                                 <div class="w3-col s6">
-                                    <input class='w3-input w3-border w3-center' type='number' id='customRange2' v-model='orderQuantity'>
+                                    <!-- CANTIDAD -->
+                                    <input class='w3-input w3-border w3-center' type='number' id='customRange2' v-model='item.orderQuantity'>
                                 </div>
                                 <div class="w3-col s3">
-                                    <button class='w3-button w3-border w3-pale-green w-100' v-on:click='orderQuantity < 50 ? orderQuantity++ : ""'>
+                                    <button class='w3-button w3-border w3-pale-green w-100' v-on:click='item.orderQuantity < 50 ? item.orderQuantity++ : ""'>
                                         <i class="fa-solid fa-angle-up"></i>
                                     </button>
                                 </div>
@@ -60,7 +61,7 @@
                     <span class="w3-badge w3-red m-1" style='cursor:pointer' v-on:click='deleteOrderElement(i)' >×</span>
                 </p>
             </div>
-            <!-- COMENTARIOS Y CLIENTES -->
+            <!-- COMENTARIOS Y FACTURA -->
             <div class="w3-row mb-3">
                 <div class="w3-threequarter">
                     <span>Comentarios</span>
@@ -121,10 +122,13 @@ export default {
     name: 'OrderCreator',
     data(){
         return {
-            orderTitle: '',
             orderVariants: [],
-            variantSelected: '',
-            orderQuantity: 1,
+            item:{
+                orderTitle: '',
+                variantSelected: '',
+                orderQuantity: 1,
+                orderPrice: 0,
+            },
             orderElements: [],
             orderComments: '',
             orderInvoice: false,
@@ -153,36 +157,34 @@ export default {
             allTooltips.forEach( t => t.style.visibility='hidden' );
         },
         checkVariants(){
-            const index = this.dishes.findIndex( i => i.title === this.orderTitle );
-            const variants = this.dishes[index].variant;
+            const index = this.dishes.findIndex( i => i.title === this.item.orderTitle );
+            const variants = this.dishes[index].variante;
             this.orderVariants.splice(0, this.orderVariants.length);
             this.variantSelected = '';
-            if( variants.length > 0 ){
-                variants.forEach( variant => {                        
-                    this.orderVariants.push( variant )
+            if( Object.keys(variants).length > 0 ){
+                Object.entries(variants).forEach( ([key, value]) => {
+                    this.orderVariants.push( {title: key, quantity: parseInt(value)} );
                 });
-                //Select first element of variants
-                //this.variantSelected = this.orderVariants[0];
             }
         },
         makeOrderElement(){
-            if( this.orderTitle ){
+            if( this.item.orderTitle ){
                 if( this.variantSelected ){
-                    const fullOrder = `${this.orderTitle}, ${this.variantSelected}`;
+                    const fullOrder = `${this.item.orderTitle}, ${this.item.variantSelected}`;
                     const index = this.orderElements.findIndex( i => i.title === fullOrder );
                     if(index < 0){
                         // El elemento no está
-                        this.orderElements.push( {title: fullOrder, quantity: this.orderQuantity} );
+                        this.orderElements.push( {title: fullOrder, quantity: this.item.orderQuantity, price: this.item.orderPrice} );
                     }else{
                         // El elemento si está
-                        this.orderElements[index].quantity = this.orderQuantity;
+                        this.orderElements[index].quantity = this.item.orderQuantity;
                     }
                 }else{
                     const idx = this.orderElements.findIndex( i => i.title === this.orderTitle );
                     if(idx < 0){
-                        this.orderElements.push( {title: this.orderTitle, quantity: this.orderQuantity} );
+                        this.orderElements.push( {title: this.item.orderTitle, quantity: this.item.orderQuantity, price: this.item.orderPrice} );
                     }else{
-                        this.orderElements[idx].quantity = this.orderQuantity;           
+                        this.orderElements[idx].quantity = this.item.orderQuantity;           
                     }
                 }
             }else{
@@ -235,14 +237,15 @@ export default {
                 };
                 this.$store.dispatch('comandas/addOrder', obj);
                 // Reset forms
-                this.orderTitle = '';
-                this.orderQuantity = 1;
+                this.item.orderTitle = '';
                 this.orderVariants.splice(0, this.orderVariants.length);
+                this.item.orderQuantity = 1;
                 this.orderElements.splice(0, this.orderElements.length);
                 this.orderComments = '';
                 this.orderInvoice = false;
                 this.guestName = '';
             }
+            console.log( this.item )
         }
     }
 
